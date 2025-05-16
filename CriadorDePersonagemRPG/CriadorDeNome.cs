b1a1f7b296d351a58b7cec0bf977c959f2ddd58e
onyxbldade33 <149265103+onyxbldade33@users.noreply.github.com>
2025-05-16 00:24:42 -0300

    commit de conclusão de função de nome

--- CriadorDePersonagemRPG/CriadorDeNome.cs
+++ CriadorDePersonagemRPG/CriadorDeNome.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,48 @@ namespace CriadorDePersonagemRPG
 {
     internal class CriadorDeNome
     {
+        public string ObterNomeDoPersonagem()
+        { // declaração das variavéis
+            string nomePersonagem = "";
+            string nomePersonagem2 = "";
+            bool nomeConfirmado = false;
+        //mensagem de boas vindas, talvez seja pertinente tirar e colocar na primeira parte do codigo que vai ser exibida no console
+            Console.WriteLine("Bem vindo safado!");
+            // inicio do loop
+            while (!nomeConfirmado)
+            {
+                Console.WriteLine("\nPor Favor, digite o nome de seu personagem");
+                nomePersonagem = Console.ReadLine();
+                // confirmação de nome
+                Console.WriteLine($"\nVocê digitou: {nomePersonagem}.");
+                Console.Write("Este nome está correto? (sim/não): ");
+                nomePersonagem2 = Console.ReadLine() .ToLower();
+
+                if (nomePersonagem2 == "sim") 
+                {
+                    nomeConfirmado=true;
+                    Console.WriteLine("\nNome confirmado!");
+                    Console.WriteLine();
+
+                }
+
+                else if (nomePersonagem2 == "não") 
+                {
+                    Console.WriteLine("\nNome incorreto. Por favor, tenta dnv ai cara.");
+                }
+
+                else
+                {
+                    Console.WriteLine("\nResposta inválida. Digite 'sim' ou 'não'.");
+
+                }
+                // fim do loop
+        
+
+            }
+
+            return nomePersonagem;
+
+        }
     }
 }
--- CriadorDePersonagemRPG/Program.cs
+++ CriadorDePersonagemRPG/Program.cs
@@ -1,17 +1,17 @@
 
-﻿using System;
+using System;
 
 namespace CriadorDePersonagemRPG;
 class Program
 {
     static void Main()
     {
-        int vida = 10; 
-        int estamina; 
-        int forca; 
-        int mana; 
-        int reflexo; 
-        int danoArma; 
+        int vida = 10;
+        int estamina;
+        int forca;
+        int mana;
+        int reflexo;
+        int danoArma;
         int atkN1;
         int atkN2;
         int atkN3;
@@ -28,5 +28,12 @@ class Program
 
         CriadorDeRaca qualquercoisa = new CriadorDeRaca();
         qualquercoisa.vida(vida);
+
+        // instância criada, chama o metodo e exibe no console
+        CriadorDeNome criadorDeNome = new CriadorDeNome();
+        nomePersonagem = criadorDeNome.ObterNomeDoPersonagem(); 
+
+        Console.WriteLine($"O nome do personagem é: {nomePersonagem}");
+        // fim
     }
 }
